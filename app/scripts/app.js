2f986ae448a7a563a879b9d09857eae4ff2b0fb2
dallas1278 <dallas.stowasser@gmail.com>
2014-06-15 08:31:07 -0500

    foo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,31 @@
 'use strict';
 
-angular.module('fooApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
+/**
+ * @ngdoc overview
+ * @name fooApp
+ * @description
+ * # fooApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fooApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+ * @ngdoc function
+ * @name fooApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fooApp
+ */
 angular.module('fooApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
